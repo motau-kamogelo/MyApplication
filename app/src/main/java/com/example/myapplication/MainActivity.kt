4f086bc6a2f87dce1737b1b51ff3b4b340a094be
Kamogelo Motau <127486770+motau-kamogelo@users.noreply.github.com>
2025-09-11 13:03:25 +0000

    Update MainActivity.kt

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Androidfdhdfg",
+                        name = "Androidfdhvbgfddfg",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
